e93707ffd1487b2e9e3451e77a393f87abbfa008
Jiashun Gou <enjoybeta@jiashun-mac.lan>
2018-04-03 02:46:46 -0400

    Initial Commit

---
+++ UrgentPath/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UrgentPath
+//
+//  Created by Jiashun Gou on 4/3/18.
+//  Copyright © 2018 Jiashun Gou. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
