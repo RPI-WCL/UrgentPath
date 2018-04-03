2a409c01d96975bd3795d80a4544913b1d8cbf4f
enjoybeta <goujiashun@gmail.com>
2018-04-03 15:56:30 -0400

    import code
    import code from TrajectoryWind(fc86aa7)
    T12 folder

---
+++ UrgentPath/Calculation/TrajectoryCal.h
@@ -0,0 +1,31 @@
+//
+//  TrajectoryCal.h
+//  UrgentPath
+//
+//  Created by Jiashun Gou on 4/3/18.
+//  Copyright © 2018 Jiashun Gou. All rights reserved.
+//
+#pragma once
+#include "demo.h"
+#include "helper.h"
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+
+
+extern int helloworld2(void);
+
+
+Seg basic_path(Packet data);
+Seg2 model_wind(Seg path_with_spiral, Packet data);
+
+externint TrajectoryCal();
--- UrgentPath/FirstViewController.swift
+++ UrgentPath/FirstViewController.swift
@@ -12,12 +12,18 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        hello_first()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func hello_first() {
+        let x = helloworld2()
+        print(x)
     }
 
 
