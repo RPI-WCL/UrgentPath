75bcc4045da59c0aafd112d99099aa5288c6dc54
enjoybeta <goujiashun@gmail.com>
2018-04-06 21:01:51 -0400

    v0.1.2
    introduce data classes and its manager

---
+++ UrgentPath/Calculation/Data/DataPlane.swift
@@ -0,0 +1,17 @@
+//
+//  dataPlane.swift
+//  UrgentPath
+//
+//  Created by Jiashun Gou on 4/6/18.
+//  Copyright © 2018 Jiashun Gou. All rights reserved.
+//
+
+import Foundation
+
+struct DataPlane{
+    var plane_type : String
+    var update_interval : Double
+    var best_gliding_airspeed : Double
+    var best_gliding_ratio : Double
+    var dirty_gliding_ratio : Double
+}
---
+++ UrgentPath/Calculation/Data/DataPlaneManager.swift
@@ -0,0 +1,45 @@
+//
+//  dataPlaneManager.swift
+//  UrgentPath
+//
+//  Created by Jiashun Gou on 4/6/18.
+//  Copyright © 2018 Jiashun Gou. All rights reserved.
+//
+
+import Foundation
+
+//let sharedDataPlaneManager = DataPlaneManager()
+
+class DataPlaneManager {
+    static let shared = DataPlaneManager()//singleton
+    
+    private var currentPlaneIndex : Int
+    private var data = [DataPlane]()
+    
+    init() {
+        currentPlaneIndex = -1
+        let config_plane_a320 = DataPlane(plane_type: "A320",
+                             update_interval: 0.001,
+                             best_gliding_airspeed: 240.0,
+                             best_gliding_ratio: 17.25,
+                             dirty_gliding_ratio: 9.0)
+        let config_plane_cessna172 = DataPlane(plane_type: "Cessna 172",
+                                          update_interval: 0.0001,
+                                          best_gliding_airspeed: 240.0,
+                                          best_gliding_ratio: 17.25,
+                                          dirty_gliding_ratio: 9.0)
+        data.append(config_plane_a320)
+        data.append(config_plane_cessna172)
+    }
+    
+    func getPlaneConfigAll() -> [DataPlane] {
+        return data
+    }
+    
+    func getPlaneConfig() -> DataPlane? {
+        if(currentPlaneIndex < 0){
+            return nil
+        }
+        return data[currentPlaneIndex]
+    }
+}
